9202adadc0a602a48c5315aaf3f9a8916f5138c5
Leonardo Souza <leonardoDevSantos@gmail.com>
2024-08-27 14:04:23 -0300

    Mudando o nome e o favicon da página

--- src/App.jsx
+++ src/App.jsx
@@ -59,6 +59,7 @@ function App() {
 
   return (
     <div className='app'>
+
       <h1>Lista de Tarefas</h1>
       <Search search={search} setSearch={setSearch}/>
       <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
